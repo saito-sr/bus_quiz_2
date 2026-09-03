24bbbb86860c0d938a56953e714dadcb070bb0cb
saito-sr <xt7nw6@bma.biglobe.ne.jp>
2026-09-03 16:04:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -193,12 +193,9 @@ quiz.forEach((q, index) => {
   summaryDiv.appendChild(p);
 });
 
-fetch(API_URL, {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({
-    name: username,
-    answers: answers,
-    score: score
-  })
-});
+fetch(API_URL + "?name=" + encodeURIComponent(username)
+    + "&answers=" + encodeURIComponent(JSON.stringify(answers))
+    + "&score=" + score, {
+      method: "GET",
+      mode: "no-cors"
+    });
